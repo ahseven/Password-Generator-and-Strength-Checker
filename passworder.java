d20eaff290998eafcc1e5c95853921e168570ab8
Rithw7 <146817425+ahseven@users.noreply.github.com>
2026-03-11 10:32:23 +0700

    Update passworder.java

--- passworder.java
+++ passworder.java
@@ -93,23 +93,35 @@ class password_strenght_check{
     
     public static String passwordStrengthRating(int strength) { 
         
+        
+        // Define the codes once at the top of your method
+        String RED = "\033[1;31m";    // Bold Red
+        String YELLOW = "\033[1;33m"; // Bold Yellow
+        String GREEN = "\033[1;32m";  // Bold Green
+        String RESET = "\033[0m";     // Back to normal
+
         // Convert strength score to a rating
         if (strength <= 2) {
-            return "Weak";
+            return RED + "Weak" + RESET;
         } 
         else if (strength <= 4) {
-            return "Medium";
+            return YELLOW + "Medium" + RESET;
         } 
         else if (strength == 5) {
-            return "Strong";
+            return GREEN + "Strong" + RESET;
         } 
         else {
             return "None"; 
         }
-        
     }
-    
     public static String common_password_check(String password) {
+
+        // Coloring text 
+        String RED_BOLD = "\033[1;31m";
+        String GREEN_BOLD = "\033[1;32m";
+        String RESET = "\033[0m";
+
+        // Wordlist to use
         Path path = Paths.get("rockyou.txt");
 
         // Processes 14+ million lines of password directly from disk to save memory
@@ -118,14 +130,15 @@ class password_strenght_check{
             // .anyMatch returns true as soon as it finds a match and STOPS reading the file.
             boolean isCommon = lines.anyMatch(line -> line.equals(password));
 
-            if (isCommon) {
-                return "\n*** WARNING: THIS IS A KNOWN COMMON PASSWORD! ***";
+           if (isCommon) {
+            // Returning the string with Red formatting "baked in"
+            return "\n" + RED_BOLD + "*** WARNING: THIS IS A KNOWN COMMON PASSWORD! STOP USING IT AND CREATE A STRONGER ONE! ***" + RESET;
             } else {
-                return "\n*** SUCCESS: Password not found in common database. ***";
+                // Returning the string with Green formatting "baked in"
+                return "\n" + GREEN_BOLD + "*** SUCCESS: Password not found in common database. ***" + RESET;
             }
-
-        } catch (IOException e) {
-            return "Error: Database file 'rockyou.txt' or any WORDLIST not found.";
+            } catch (IOException e) {
+            return RED_BOLD + "Error: Database file 'rockyou.txt' not found." + RESET;
         }
     }
 
